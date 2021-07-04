5732ebdc514811c4bbb4ce881b844e3a5a654269
irfu-matlab <ablotekar@gmail.com>
2021-07-05 00:47:34 +0200

    new functions added

--- fluidplasma/__init__.py
+++ fluidplasma/__init__.py
@@ -12,3 +12,5 @@ from .gaussian import gaussian
 from .fd4 import fd4
 from .filter3p import filter3p
 from .continuity_equation import continuity_equation
+from .kappa_density import kappa_density
+from .poissons_solution import poissons_solution
