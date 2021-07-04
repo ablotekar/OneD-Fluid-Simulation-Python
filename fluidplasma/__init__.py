980a0f322fb66701b64632448dffee2f8f95c82f
irfu-matlab <ablotekar@gmail.com>
2021-07-04 23:16:10 +0200

    First commit

--- fluidplasma/__init__.py
+++ fluidplasma/__init__.py
@@ -6,4 +6,9 @@ Date  : 2021-07-04
 
 DESC  :
 """
+
 from .periodic_boundary import periodic_boundary
+from .gaussian import gaussian
+from .fd4 import fd4
+from .filter3p import filter3p
+from .continuity_equation import continuity_equation
---
+++ ion_acoustic_solitary_wave_two_componunt_plasma.py
@@ -0,0 +1,63 @@
+"""
+Name  : ion_acoustic_solitary_wave_two_componunt_plasma.py
+Author: Ajay Lotekar
+e-mail: ablotekar@gmail.com
+Date  : 2021-07-03 
+
+DESC  : This program simulate evolution of the ion acoustic solitary
+        tow componunt plasma system. The plasma system consist of fluid
+        ions and superthermal electrons. The electron follows the Kappa
+        velocity distribution hence its density is given by kappa
+        density function and ion are governed by fluid equation.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import fluidplasma as fp
+
+# Input parameters
+lx = 1000  # System length
+dx = 0.2  # Spatial grid size
+dt = 0.1  # Temporal grid size
+ne0 = 1.0  # Equilibrium electron density
+ni0 = 1.0  # Equilibrium ion density
+nx = int(lx / dx)  # Number of grid points
+x0 = 0.5 * nx * dx  # Position of the perturbation
+wd = 10  # Width of the perturbation
+a0 = 0.5  # Amplitude of perturbation (same in this case)
+
+nx4 = nx + 4
+
+x = np.linspace(-1, nx4 - 2, nx4) * dx
+x[0] = 0.0
+x[1] = 0.0
+x[-1] = 0.0
+x[-2] = 0.0
+
+# Initializing variables
+ne = np.zeros((nx4, 2))  # Electron density
+ni = np.zeros((nx4, 2))  # Ion density
+vi = np.zeros((nx4, 2))  # Ion velocity
+ph = np.zeros((nx4, 2))  # Electrostatic potential
+Ex = np.zeros((nx4, 1))  # Electric field
+
+# Perturbing system
+ne[:, 0] = 1 + fp.gaussian(x, a0, wd, x0)
+ne[:, 1] = 1 + fp.gaussian(x, a0, wd, x0)
+
+ni[:, 0] = 1 + fp.gaussian(x, a0, wd, x0)
+ni[:, 1] = 1 + fp.gaussian(x, a0, wd, x0)
+
+# computing electrostatic potential when system get perturb
+for i in range(3, nx4 - 3):
+    ph[i + 1, 0] = 2 * ph[i, 0] - ph[i - 1, 0] \
+                   - dx * dx * (ni[i, 0] - ne[i, 0])
+ph[:, 1] = ph[:, 0]
+
+# Continuity equation
+vi, ni = fp.continuity_equation(vi, ni, ph, 1, dx, dt)
+
+
+plt.plot(x[2:nx4 - 3], ni[2:nx4 - 3, 1])
+# plt.xlim(400, 600)
+# plt.ylim(-2, 2)
+plt.show()
