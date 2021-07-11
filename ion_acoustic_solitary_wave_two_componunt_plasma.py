da93cb98bfb735efe2073eb1f7b1fd01a3601bc3
irfu-matlab <ablotekar@gmail.com>
2021-07-11 21:15:38 +0200

    Removed unwanted comments and compleated TODO

--- ion_acoustic_solitary_wave_two_componunt_plasma.py
+++ ion_acoustic_solitary_wave_two_componunt_plasma.py
@@ -29,8 +29,7 @@ kp = 2
 w = 0.70
 tol = 1.e-7
 ntime = 5000
-
-nx4 = nx + 4
+nx4 = nx + 4  # Extra grid points to update the periodic boundary
 
 x = np.linspace(-1, nx4 - 2, nx4) * dx
 x[0] = 0.0
@@ -39,10 +38,10 @@ x[-1] = 0.0
 x[-2] = 0.0
 
 # Initializing variables
-ne = np.array(np.zeros((nx4, 2)), dtype='float32') # Electron density
+ne = np.array(np.zeros((nx4, 2)), dtype='float32')  # Electron density
 ni = np.array(np.zeros((nx4, 2)), dtype='float32')  # Ion density
 vi = np.array(np.zeros((nx4, 2)), dtype='float32')  # Ion velocity
-ph = np.array(np.zeros((nx4, 2)), dtype='float32')  # Electrostatic potential
+ph = np.array(np.zeros((nx4, 2)), dtype='float32')  # Potential
 Ex = np.array(np.zeros((nx4, 1)), dtype='float32')  # Electric field
 
 # Perturbing system
@@ -52,42 +51,32 @@ ne[:, 1] = 1 + fp.gaussian(x, a0, wd, x0)
 ni[:, 0] = 1 + fp.gaussian(x, a0, wd, x0)
 ni[:, 1] = 1 + fp.gaussian(x, a0, wd, x0)
 
-# computing electrostatic potential when system get perturb
+# Computing electrostatic potential when system get perturb
 for i in range(3, nx4 - 3):
     ph[i + 1, 0] = 2 * ph[i, 0] - ph[i - 1, 0] \
                    - dx * dx * (ni[i, 0] - ne[i, 0])
 ph[:, 1] = ph[:, 0]
 
-# plt.ion()
-# figure, ax = plt.subplots(figsize=(10, 8))
-# line1, = ax.plot(x[2:nx4 - 3], ph[2:nx4 - 3, 0])
-
 for j in range(0, ntime):
-    # TODO: Remove numerical error
-    # TODO: Check for the periodic boundary update and filter
-    # TODO: Check Poisson solver, this mya be messing up.
+    # TODO: small numerical error still there neeed to remove
     # Continuity equation
     vi, ni = fp.continuity_equation(vi, ni, ph, 1, dx, dt)
     # Poisson's equation solution (kappa density)
     ph[:, 1] = fp.poissons_solution(ph[:, 1], ni[:, 1], kp, dx, w, tol)
-    ph[:, 0] = ph[:, 1]
+    ph[:, 0] = ph[:, 1]  # Initial guess
 
     # Continuity equation
     vi, ni = fp.continuity_equation(vi, ni, ph, 0, dx, dt)
     # Poisson's equation solution (kappa density)
     ph[:, 0] = fp.poissons_solution(ph[:, 0], ni[:, 0], kp, dx, w, tol)
-    ph[:, 1] = ph[:, 0]
+    ph[:, 1] = ph[:, 0]  # Initial guess
 
-    # Plot the results
-    # line1.set_ydata(ph[2:nx4 - 3, 0])
-    # figure.canvas.draw()
-    # figure.canvas.flush_events()
-
+    # Plotting or saving the result
     if np.mod(j, 50) == 0:
         # TODO: Need to find way to reused same figure window
         plt.plot(x[2:nx4 - 3], ph[2:nx4 - 3, 0])
         # plt.xlim(400, 600)
         # plt.ylim(-2, 2)
-        plt.title("Linear graph" + str(j))
+        plt.title("Iteration = " + str(j))
         plt.show()
         tp.sleep(0.5)
